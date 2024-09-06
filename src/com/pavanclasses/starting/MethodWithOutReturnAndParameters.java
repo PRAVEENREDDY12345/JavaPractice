cbe730f9ba78568b6dca5d7f54e868e946aefb48
PRAVEENREDDY12345 <87979325+PRAVEENREDDY12345@users.noreply.github.com>
2024-09-06 08:22:37 +0530

    Initial commit

---
+++ src/com/pavanclasses/starting/MethodWithOutReturnAndParameters.java
@@ -0,0 +1,18 @@
+package com.pavanclasses.starting;
+
+public class MethodWithOutReturnAndParameters {
+
+	
+	public void m2() {
+		
+		System.out.println("This is the method with out return and Parameters");
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MethodWithOutReturnAndParameters method = new MethodWithOutReturnAndParameters();
+		method.m2();
+	}
+
+}
