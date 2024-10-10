17205b88c1f68a1f86a6d6b15f4eaffad6ae2728
PRAVEENREDDY12345 <87979325+PRAVEENREDDY12345@users.noreply.github.com>
2024-10-10 12:53:06 +0530

    added new lasses

---
+++ src/com/pavanclasses/collections/InsertionOrder.java
@@ -0,0 +1,33 @@
+package com.pavanclasses.collections;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class InsertionOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+List l = new ArrayList();	
+ l.add("Praveen");
+ l.add("Bhanu");
+ l.add("Abbas");
+ l.add("Naresh");
+ System.out.println(l);
+ 
+ 
+ System.out.println("*********************************************");
+ 
+ Set s = new HashSet();
+ 
+ s.add("Praveen");
+ s.add("Bhanu");
+ s.add("Abbas");
+ s.add("Naresh");
+ 
+	System.out.println(s);
+	
+	}
+
+}
---
+++ src/com/pavanclasses/collections/Sorting.java
@@ -0,0 +1,16 @@
+package com.pavanclasses.collections;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Sorting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Set s = new TreeSet()
+
+
+;
+}
+
+}
